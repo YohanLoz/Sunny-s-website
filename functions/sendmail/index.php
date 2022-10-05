f9374794df62fd29f73abefae597d8798dd54667
Zsombor <zsombor.hajzer@student.nhlstenden.com>
2022-10-05 15:47:03 +0100

    Newsletter function version 1 is added

---
+++ functions/sendmail/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<form class = "" action ="send.php" method="post">
+
+    Email <input type="email" 
+    name ="email" value=""><br>
+
+        <button type="submit" name="send">Send</button>
+
+</body>
+</html>
---
+++ functions/sendmail/send.php
@@ -0,0 +1,40 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if(isset($_POST["send"])){
+    $mail = new PHPMailer(true);
+
+
+    $mail->isSMTP();
+    $mail->Host ="smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = 'Sunny.Socks.Newsletter@gmail.com';
+    $mail->Password ='gdecqaomrikacloh';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+    
+
+
+    $mail->setFrom('Sunny.Socks.Newsletter@gmail.com');
+    $mail->addAddress($_POST["email"]);
+    $mail->isHTML(true);
+    $mail->Subject = "Socks On SALE ???????
+    NO WAYYYYY!!!!!";
+    // $_POST["subject"];
+    $mail->Body = "BUY ONE SOCK GET THREE SOCKS. WE ARE SELLING SOCKS FOR FREE BASICALLY. HELP OUT YOUR COMMUNITY BY BUYING THESE AMAZING FAIR TRADE VEGAN SOCKS. YOU CAN ALSO SAVE TURTULES SINCE IT IS NOT MADE OF PLASTIC. IT IS CRAZY HOW GOOD THIS DEAL IS. YOU ARE BASICALLY ROBBING US. I COULD CALL THE POLICE. HMMMMMM I REALLY SHOULD CALL THE POLICE. I WILL CALL THE POLICE YOU NASTY CRIMINAL!!!!";
+    // $_POST["message"];
+    $mail->send();
+    echo "
+    <script>
+    alert('sent succesfully')
+    </script>
+    ";
+}
+
+
+?>
