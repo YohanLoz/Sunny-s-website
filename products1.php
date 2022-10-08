5bc6cd5792da036912b81cb9bfc92d4bba463e85
Yohan Lozanov <yohan.lozanov@student.nhlstenden.com>
2022-10-08 09:56:08 +0200

    added everything except for the pictures to the products1 page, as well as adding the main.css which will serve as the backbone for all pages on the site

---
+++ products1.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Sunny's products</title>
+        <link rel="stylesheet" href="css/main.css"/>
+        <link rel="stylesheet" href="css/products1.css"/>
+    </head>
+
+    <body>
+        <header>
+            <h1 style="background-color:coral;margin:0;"">this is the head (for now)</h1>
+        </header>
+
+        <section id="title">
+            <h1>Meet the classics.</h1>
+            <p>
+                We like to add some color to one of the the most <br/>
+                everyday moments of life: putting on your socks.<br/>
+                We add literal color by designing simple basic<br/>
+                white socks a pop of color.
+            </p>
+        </section>
+
+        <section id="classicStripes" class="product">
+            
+            <h2>classic:<br/>stripes.</h2>
+            
+            <div class="scrollingsocks"><img/></div>
+            
+            <div class="linkcontainer">
+                <a href="">learn more ></a>
+                <br/>
+                <a href="">our webshop ></a>
+            </div>
+
+        </section>
+
+        <section id="classicUni" class="product">
+            <h2>classic:<br/>uni color.</h2>
+            
+            <div class="scrollingsocks"><img/></div>
+
+            <div class="linkcontainer">
+                <a href="">learn more ></a>
+                <br/>
+                <a href="">our webshop ></a>
+            </div>
+        </section>
+
+        <section id="resellers" class="white">
+
+            <h2>Our<br/>Resellers:</h2>
+            <p>
+                Sunny uses resellers.<br/>
+                You can buy your socks from these sites:
+            </p>
+
+
+            <div id="germanResell">
+                <h3>in<br/>Germany:</h3>
+                <a href="">amazon.de ></a>
+            </div>
+
+            <div id="dutchResell">
+                <h3>in the<br/>Netherlands:</h3>
+                <a href="">hema.com ></a>
+                <a href="">bol.com ></a>
+            </div>
+
+        </section>
+
+    </body>
+ 
+</html>
