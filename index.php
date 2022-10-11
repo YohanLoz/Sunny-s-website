9f8b96851f7d3ed348d1ebcc9b1faf555659df9c
beyza olmez <beyza.olmez@student.nhlstenden.com>
2022-10-11 09:42:06 +0200

    added html and css for the mainpage of the website. created a folder for images used.

--- index.php
+++ index.php
@@ -1,14 +1,85 @@
 <!doctype html>
 <html>
 <head>
-<title> </title>
+	<meta charset="UTF8"/>
+    <title> Main Page </title>
+	<link rel= "stylesheet" href="css/MainPageCss.css"/>
 </head>
-
 <body>
+<div id= "MainContainer" >
+	<header>
+	<p>  header </p>
+	</header>
+	
+	<h1> The bright <br> side of socks. </h1>
 
-    <h1> HEllo worDL </h1>
+	<img class="img1" src="img/corap fotosu.jpg" alt="picture of sunny sock" >
+	<p class="p1"> We like to add some color 
+	to one of the the most
+	everyday moments of life: 
+	putting on your socks.
+	We add literal color by 
+	designing simple basic
+	white socks a pop of color. </p>
 
-    <h1> Hello website </h1>
+<!-- newsletter scroll down button -->
+<p class="newsletterbutton"> newsletter </p>
 
+<!-- background sunny blue start -->
+	<div id="blueframe">
+		<h2> We like win-win <br> situations. </h2>
+
+			<img src="" alt="" >
+			<p class="p2"> When we produce and sell 
+			our socks it is beneficial for 
+			everyone involved.
+
+			The raw materials are 
+			gained in such a way it 
+			does not harm the earth. 
+			The production is done 
+			with the environment in 
+			mind and with an honest 
+			waige for the people 
+			working in the factories.
+			The shops that sell our 
+			sock make a fair provit. 
+
+			And last but not least: the 
+			costumer gets high quality 
+			socks for a fair price. </p>
+	</div>
+<!-- background sunny blue end -->
+
+
+	<h3 id="mission"> Our mission: </h3>
+	<h3 id="vision" > Our vision: </h3>
+
+	<!-- mission paragraph --> 
+	<p id="Pmission"> Sunny’s mission is to brighten
+	peoples day by providing high               
+	quality, ethically produced 
+	socks. </p>
+
+	<!-- vision paragraph -->
+	<p id="Pvision"> Sunny’s vision is to produce 
+	ethical and sustaineble socks 
+	that are available for 
+	everybody </p>
+
+<!-- pink box -->
+<div class="pinkbox" >
+	<h4> Learn more by subscribing to our newsletter! </h4>
+	
+	<p id="email"> Email: </p> 
+	<p id="subscribebutton"> Subscribe for free </p>
+	<p id="pinktext"> No strings attached - Unsubsribe anytime </p>
+</div>
+<!-- pink box end -->
+
+
+<!-- newsletter scroll up button -->
+
+</div>
 </body>
 </html>
