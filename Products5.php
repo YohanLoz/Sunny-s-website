fa3dba6fe145c7c75016dfe4f298bc6543f95c1c
AlikhanSmagulov <111963762+AlikhanSmagulov@users.noreply.github.com>
2022-10-14 16:18:34 +0300

    Add files via upload

---
+++ Products5.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css"
+    <title> </title>
+</head>
+<body id="container">
+<header>
+
+</header>
+<img src="Arrow-29.jpg" class="arrow">
+<div class="main-container">
+
+    <div class="slogan-position">
+        <h1 class="slogan">classic: uni color.</h1>
+    </div>
+
+    <div class="top-container">
+        <div class="box1" >
+            <img src="ss%20stripe%20pink.jpg" class="img">
+            <h1>&nbsp;&nbsp;o u r &nbsp;&nbsp;&nbsp;w e b s h o p&nbsp;&nbsp;&nbsp;></h1>
+        </div>
+
+        <div class="box2"
+        <p class="text">Made from 100% organic cotton Produced in Portugal</p>
+        <br><br/>
+        <h3>Available in:</h3>
+        <div class="buttons">
+            <div class="blue"></div>
+            <div class="green"></div>
+            <div class="pink"></div>
+            <div class="orange"></div>
+            <div class="yellow"></div>
+
+        </div>
+    </div>
+</div>
+
+<div class="bottom-container"
+<div class="bottom1000-7">
+    <h1 class="bottom1">&nbsp;&nbsp;&nbsp;&nbsp;Our Resellers</h1>
+    <p class="bottom2">Sunny uses resellers.</p>
+    <p class="bottom2">You can buy your socks from these sites:</p>
+    <p class="bottom3">In Germany:</p>
+    <p class="bottom4">in the Netherlands:</p>
+    <p class="bottom5"><a href="https://www.amazon.de/"> amazon.de ></p>
+    <p class="bottom6"><a href="https://www.hema.com/" > hema.com ><a href="https://www.bol.com/nl/nl/"> bol.com ></p>
+</div>
+</div>
+</body>
+</html>
