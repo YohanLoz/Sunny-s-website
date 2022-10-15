3263a64195ec47c751abfa95d7f850145d60ce5f
Zsombor <zsombor.hajzer@student.nhlstenden.com>
2022-10-15 19:28:21 +0100

    Contact page started

--- contact.php
+++ contact.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact</title>
-<link rel="stylesheet" href="css/main.css">
-</head>
-<body>
-
-<p>This is a test </p>
-    
-</body>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Contatcs page</title>
+        <link rel="stylesheet" href="css/main.css"/>
+        <link rel="stylesheet" href="css/contact.css"/>
+    </head>
+    <body>
+        <header>
+            <h1 style="background-color:coral;margin:0;">this is the head (for now)</h1>
+        </header>
+    </body>
 </html>
