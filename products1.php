74a3b5cfc8ee62642a2d46fedaf4390527dd7e17
Yohan Lozanov <yohan.lozanov@student.nhlstenden.com>
2022-10-15 12:17:42 +0200

    added the mobile layout to products1 as well as updating the main css file for mobile use

--- products1.php
+++ products1.php
@@ -14,45 +14,59 @@
 
         <section id="title">
             <h1>Meet the classics.</h1>
-            <p>
+            <p class="pcOnly">
                 We like to add some color to one of the the most <br/>
                 everyday moments of life: putting on your socks.<br/>
                 We add literal color by designing simple basic<br/>
                 white socks a pop of color.
+            </p>
+            <p class="mobileOnly">
+                We like to add some color to<br/>
+                one of the the most everyday<br/>
+                moments of life:<br/>
+                putting on your socks.<br/>
+                <br/>
+                We add literal color by designing<br/>
+                simple basic white socks a pop<br/>
+                of color.
             </p>
         </section>
 
         <section id="classicStripes" class="product">
             
             <h2>classic:<br/>stripes.</h2>
+
+            <a href="" class="mobileOnly">learn more ></a>
             
             <div class="scrollingsocks">
                 <div>
                     <img src="img/classicStripedSocks.png" alt="striped socks"/> 
-                    <img src="img/classicStripedSocks.png" alt="striped socks"/>
+                    <img src="img/classicStripedSocks.png" alt="striped socks" class="pcOnly"/>
                 </div>
             </div>
             
-            <div class="linkcontainer">
+            <div class="linkcontainer pcOnly">
                 <a href="">learn more ></a>
                 <br/>
-                <a href="">our webshop ></a>
+                <a href="" class="pcOnly">our webshop ></a>
             </div>
 
         </section>
 
         <section id="classicUni" class="product">
+
+            <a href="" class="mobileOnly">learn more ></a>
             <h2>classic:<br/>uni color.</h2>
             
             <div class="scrollingsocks">
                 <div>
                     <img src="img/classicUniColorSocks.png" alt="unicolor socks"/>
-                    <img src="img/classicUniColorSocks.png" alt="unicolor socks"/>
+                    <img src="img/classicUniColorSocks.png" alt="unicolor socks" class="pcOnly"/>
                 </div>
             </div>
 
             <div class="linkcontainer">
-                <a href="">learn more ></a>
+                <a href="" class="pcOnly">learn more ></a>
                 <br/>
                 <a href="">our webshop ></a>
             </div>
@@ -75,6 +89,7 @@
             <div id="dutchResell">
                 <h3>in the<br/>Netherlands:</h3>
                 <a href="">hema.com ></a>
+                <br/>
                 <a href="">bol.com ></a>
             </div>
 
