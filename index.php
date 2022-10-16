3b044b78b16b0bba83b3b03045c89f7cf0a9e493
beyza olmez <beyza.olmez@student.nhlstenden.com>
2022-10-16 21:43:27 +0200

    added content to the flexboxes. fixed h3 and it's paragraphs. fixed the newsletter box.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF8">
 	<title> Sunny Socks | Main Page </title>
-	<link rel= "stylesheet" href="css/index.css">
+	<link rel= "stylesheet" href="css/index4.css">
 </head>
 
 <body>
@@ -13,24 +13,51 @@
 			<p> temporary header </p>
 		</header>
 		
-		<h1> The bright <br> side of socks </h1>
+		<h1> The bright side of socks. </h1>
 		
 		<div class="sockandp">
 		
-			<div class="flex-item-left">1</div> <!-- img -->
-			<div class="flex-item-right">2</div> <!-- p -->
+			<div class="flex-item-left">  <img src="img/img1.jpg" alt="image of a sock"> </div> 
+			<div class="flex-item-right"> <p> We like to add some color 
+											to one of the the most
+											everyday moments of life:
+											putting on your socks. <br> <br>
+			
+											We add literal color by 
+											designing simple basic
+											white socks a pop of color. </p> 
+											
+			<div class="scrolldown"> <p> Our Newsletter ˇ </div>
+											
+			</div> 
 	
+
 		</div>
-		
-		<div class="arrow1"> <p> Our Newsletter </p> </div>
 	
 	<div class="blueframe">
-		<h2> We like win-win <br> situations </h2>
+		<h2> We like win-win <br> situations. </h2>
 		
 		<div class="fishandp">
 		
-			<div class="flex-item-left2">1</div> <!-- img -->
-			<div class="flex-item-right2">2</div> <!-- p -->
+			<div class="flex-item-left2"> <img src="img/img2.jpg" alt="image of a fish"> </div> 
+			<div class="flex-item-right2"> <p> When we produce and sell 
+											our socks it is beneficial for 
+											everyone involved. <br> <br>
+
+											The raw materials are 
+											gained in such a way it 
+											does not harm the earth. 
+											The production is done 
+											with the environment in 
+											mind and with an honest 
+											waige for the people 
+											working in the factories. <br>
+											The shops that sell our 
+											sock make a fair provit. <br> <br>
+
+											And last but not least: the 
+											costumer gets high quality 
+											socks for a fair price. </p> </div> 
 	
 		</div>
 		
@@ -40,7 +67,7 @@
 		<h3> Our mission: </h3>
 		
 		<p> Sunny’s mission is to brighten
-			peoples day by providing high               
+			peoples day by providing high             
 			quality, ethically produced 
 			socks. </p>
 	</div>
@@ -66,8 +93,7 @@
 		</div> 
 	</div>
 	
-	<div class="arrow2">
-	</div>
+	<div class="scrollup"> <p> Scroll up Button ^ </div>
 
 	</div>
 </body>
