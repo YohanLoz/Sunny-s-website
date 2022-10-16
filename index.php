b7ba446ade0a58e560b2e763bdfbabef5906b181
beyza olmez <beyza.olmez@student.nhlstenden.com>
2022-10-16 17:54:34 +0200

    updated the html of the mainpage for it to respond to the css

--- index.php
+++ index.php
@@ -2,37 +2,46 @@
 <html>
 <head>
 	<meta charset="UTF8"/>
-    <title> Main Page </title>
-	<link rel= "stylesheet" href="css/MainPageCss.css"/>
+	<title> Sunny Socks | Main Page </title>
+	<link rel= "stylesheet" href="css/index.css">
 </head>
+
 <body>
-<div id= "MainContainer" >
-	<header>
-	<p>  header </p>
-	</header>
+	<div id="container">
 	
-	<h1> The bright <br> side of socks. </h1>
+	<!-- header goes here -->
+	<header> <p> nbdhjk </p> </header>
+	
+			<h1> The bright <br> side of socks. </h1>
+			
 
-	<img class="img1" src="img/corap fotosu.jpg" alt="picture of sunny sock" >
-	<p class="p1"> We like to add some color 
-	to one of the the most
-	everyday moments of life: 
-	putting on your socks.
-	We add literal color by 
-	designing simple basic
-	white socks a pop of color. </p>
+			<img class="img1" src="img/img1.jpg" alt="image 1"> 
+		
+			<p class="p1"> We like to add some color 
+			to one of the the most
+			everyday moments of life:
+			putting on your socks. <br> <br>
+			
+			We add literal color by 
+			designing simple basic
+			white socks a pop of color. </p>
 
-<!-- newsletter scroll down button -->
-<p class="newsletterbutton"> newsletter </p>
+		
+<!-- 
+<div class="scrolldown button">
+	<p> Our Newsletter </p>
+</div> 
+-->			
+			<!-- blue frame START -->
+		<div id="blueframe"> 
+		
+			<h2> We like win-win <br> situations. </h2>
 
-<!-- background sunny blue start -->
-	<div id="blueframe">
-		<h2> We like win-win <br> situations. </h2>
-
-			<img src="" alt="" >
+			<img class="img2" src="img/img2.jpg" alt="picture of fish" >
+			
 			<p class="p2"> When we produce and sell 
 			our socks it is beneficial for 
-			everyone involved.
+			everyone involved. <br> <br>
 
 			The raw materials are 
 			gained in such a way it 
@@ -43,43 +52,46 @@
 			waige for the people 
 			working in the factories.
 			The shops that sell our 
-			sock make a fair provit. 
+			sock make a fair provit. <br> <br>
 
 			And last but not least: the 
 			costumer gets high quality 
 			socks for a fair price. </p>
-	</div>
-<!-- background sunny blue end -->
+			
+		 </div>
+			<!-- blue frame END   -->
+			
+		<div id="whiteframe">
+		
+			<h3 id="mission"> Our mission: </h3>
+			<h3 id="vision" > Our vision: </h3>
+			
+		<!-- mission paragraph --> 
+			<p id="Pmission"> Sunny’s mission is to brighten
+			peoples day by providing high               
+			quality, ethically produced 
+			socks. </p>
 
+		<!-- vision paragraph -->
+			<p id="Pvision"> Sunny’s vision is to produce 
+			ethical and sustainable socks 
+			that are available for 
+			everybody </p>
 
-	<h3 id="mission"> Our mission: </h3>
-	<h3 id="vision" > Our vision: </h3>
-
-	<!-- mission paragraph --> 
-	<p id="Pmission"> Sunny’s mission is to brighten
-	peoples day by providing high               
-	quality, ethically produced 
-	socks. </p>
-
-	<!-- vision paragraph -->
-	<p id="Pvision"> Sunny’s vision is to produce 
-	ethical and sustaineble socks 
-	that are available for 
-	everybody </p>
-
-<!-- pink box -->
-<div class="pinkbox" >
-	<h4> Learn more by subscribing to our newsletter! </h4>
+		<div class="pinkbox" >
+			<h4> Learn more by subscribing to our newsletter! </h4>
+			
+		<ul id="buttons">
+			<li id="email"> Email: </li>
+			<li id="subscribebutton"> Subscribe for free </li>
+		</ul> 
 	
-	<p id="email"> Email: </p> 
-	<p id="subscribebutton"> Subscribe for free </p>
-	<p id="pinktext"> No strings attached - Unsubsribe anytime </p>
-</div>
-<!-- pink box end -->
-
-
+			<p id="pinktext"> No strings attached - Unsubscribe anytime </p>
+		</div> 
+		</div>
 <!-- newsletter scroll up button -->
 
-</div>
+		</div>
+	</div>
 </body>
 </html>
