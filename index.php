5befb1c8a1bec1a03f7a9d203ed6905b471228f9
beyza olmez <beyza.olmez@student.nhlstenden.com>
2022-10-16 19:25:22 +0200

    added flexboxes to the mainpage

--- index.php
+++ index.php
@@ -1,84 +1,60 @@
 <!doctype html>
 <html>
 <head>
-	<meta charset="UTF8"/>
+	<meta charset="UTF8">
 	<title> Sunny Socks | Main Page </title>
 	<link rel= "stylesheet" href="css/index.css">
 </head>
 
 <body>
 	<div id="container">
+
+		<header>
+			<p> temporary header </p>
+		</header>
+		
+		<h1> The bright <br> side of socks </h1>
+		
+		<div class="sockandp">
+		
+			<div class="flex-item-left">1</div> <!-- img -->
+			<div class="flex-item-right">2</div> <!-- p -->
 	
-	<!-- header goes here -->
-	<header> <p> nbdhjk </p> </header>
+		</div>
+		
+		<div class="arrow1"> <p> Our Newsletter </p> </div>
 	
-			<h1> The bright <br> side of socks. </h1>
-			
-
-			<img class="img1" src="img/img1.jpg" alt="image 1"> 
+	<div class="blueframe">
+		<h2> We like win-win <br> situations </h2>
 		
-			<p class="p1"> We like to add some color 
-			to one of the the most
-			everyday moments of life:
-			putting on your socks. <br> <br>
-			
-			We add literal color by 
-			designing simple basic
-			white socks a pop of color. </p>
-
+		<div class="fishandp">
 		
-<!-- 
-<div class="scrolldown button">
-	<p> Our Newsletter </p>
-</div> 
--->			
-			<!-- blue frame START -->
-		<div id="blueframe"> 
+			<div class="flex-item-left2">1</div> <!-- img -->
+			<div class="flex-item-right2">2</div> <!-- p -->
+	
+		</div>
 		
-			<h2> We like win-win <br> situations. </h2>
-
-			<img class="img2" src="img/img2.jpg" alt="picture of fish" >
-			
-			<p class="p2"> When we produce and sell 
-			our socks it is beneficial for 
-			everyone involved. <br> <br>
-
-			The raw materials are 
-			gained in such a way it 
-			does not harm the earth. 
-			The production is done 
-			with the environment in 
-			mind and with an honest 
-			waige for the people 
-			working in the factories.
-			The shops that sell our 
-			sock make a fair provit. <br> <br>
-
-			And last but not least: the 
-			costumer gets high quality 
-			socks for a fair price. </p>
-			
-		 </div>
-			<!-- blue frame END   -->
-			
-		<div id="whiteframe">
+	</div>
+	
+	<div class="mission">
+		<h3> Our mission: </h3>
 		
-			<h3 id="mission"> Our mission: </h3>
-			<h3 id="vision" > Our vision: </h3>
-			
-		<!-- mission paragraph --> 
-			<p id="Pmission"> Sunny’s mission is to brighten
+		<p> Sunny’s mission is to brighten
 			peoples day by providing high               
 			quality, ethically produced 
 			socks. </p>
-
-		<!-- vision paragraph -->
-			<p id="Pvision"> Sunny’s vision is to produce 
+	</div>
+	
+	<div class="vision">
+		<h3> Our vision: </h3>
+	
+		<p> Sunny’s vision is to produce 
 			ethical and sustainable socks 
 			that are available for 
 			everybody </p>
-
-		<div class="pinkbox" >
+	</div>
+	
+	<div class="pinkbox" >
 			<h4> Learn more by subscribing to our newsletter! </h4>
 			
 		<ul id="buttons">
@@ -88,10 +64,11 @@
 	
 			<p id="pinktext"> No strings attached - Unsubscribe anytime </p>
 		</div> 
-		</div>
-<!-- newsletter scroll up button -->
+	</div>
+	
+	<div class="arrow2">
+	</div>
 
-		</div>
 	</div>
 </body>
 </html>
