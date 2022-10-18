0314cfcd8e9c24a8a291d895138200b5dba28ab1
BivolMaksym <112406077+BivolMaksym@users.noreply.github.com>
2022-10-18 19:29:58 +0200

    Add files via upload

--- products2.php
+++ products2.php
@@ -1,12 +1,55 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-<title> </title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/product2style.css">
+<title>
+	Products
+</title>
 </head>
+<body id="container">
+<header>
+		
+</header>
+<img src="img/Arrow-29.jpg" class="arrow">
+<div class="maincontainer">
+	
+<div class="slogandiv">
+	 <h1 class="slogan">classic: stripes.</h1>
+</div>
+<div class="container">
+<div class="box1">
+	<img src="img/ss stripe pink.jpg" class="heroimage">
+	<p class="ourwebshop a">&nbsp;&nbsp;o u r &nbsp;&nbsp;&nbsp;w e b s h o p&nbsp;&nbsp;&nbsp;></p>
+	
+</div>
+<div class="box2">
+	<a class="herotext">Made from 100% organic cotton Produced in Portugal</a>
+	<h4 class="available">Available in:</h4>
+	<div class="container2">
+		<div class="blue"></div>
+		<div class="green"></div>
+		<div class="pink"></div>
+		<div class="orange"></div>
+		<div class="yellow"></div>
 
-<body>
+	</div>
+</div>
+</div>
 
-    <h1> products 2 stripes </h1>
-
+<footer class="footer">
+	<div class="divfooter">
+	<h1 class="footerh1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Our Resellers</h1>
+	<p class="footerh2">Sunny uses resellers.</p>
+	<p class="footerh2">You can buy your socks from these sites:</p>
+	<p class="footerp">In Germany:</p>
+	<p class="footerp2">in the Netherlands:</p>
+	<p class="footerp3">amazon.de ></p>
+	<p class="footerp4">hema.com > bol.com ></p>
+    </div>
+</footer>
+	</div>
 </body>
 </html>
