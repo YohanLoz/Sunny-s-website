5c08e869eb98ea18545b06dcbba4f62d8ecb37ca
BivolMaksym <112406077+BivolMaksym@users.noreply.github.com>
2022-10-28 01:10:56 +0200

    Add files via upload

---
+++ products2.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sunny</title>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="css/main.css"/>
+    <link rel="stylesheet" href="css/product2style.css"/>
+</head>
+<body>
+    <header>
+          <div class="container">
+        <div class="grid">
+                
+                
+                <img id="logo"src="img/sunny_logos_slogan_blue.png"  alt="logo">
+                <a href="index.html" class="toplink1">Home page</a>
+                <a href="index.html#blueframe" class="toplink2">About Us</a>
+                <a href="products1.php" class="toplink3">Products</a>
+                <a href="contact.php" class="toplink4">Contact</a>
+                    
+                <button  class="mobilebutton"><div class="circle"></div></button>
+                
+            </div>
+        </div>
+    </header>
+
+    <div id="dropdown" class="mobilemeniu">
+        <ul class="links">
+              <li ><a id="up" href="index.html">> Home page</a></li>
+              <li><a  href="products1.php">> Products</a></li>
+              <li ><a id="down" href="contact.php">> Contact</a></li>
+        </ul>
+    </div>
+ <div class="arrow">
+	<img src="img/Arrow-29.png" class="arrow">
+ </div>
+<div id="title">
+   <h1>classic stripes</h1>
+</div>
+
+<section id="hero">  
+    <div id="socks" class="javasocks">
+        <img src="img/Sunny_socks_pink.jpg" class="socks1" id="img">
+        <p class="mobilemade">
+            Made from 100% organic cotton
+            Produced in Portugal
+            </p>
+    </div>
+    <div id="sockstext">
+
+          <div class="mobiletext">
+            <p>Made from 100% organic cotton </br>Produced in Portugal</p>
+          </div>
+          <div>
+            <p class="boldp">Available in:</p>
+          </div>
+          <div class="circles">
+            <div class="color color-blue"></div>
+            <div class="color color-green"></div>
+            <div class="color color-pink"></div>
+            <div class="color color-red"></div>
+            <div class="color color-yellow"></div>
+            <script src="functions/colours-change.js"></script>
+        </div>
+    </div>
+
+</section>
+<br></br>
+<div id="ourwebshop">
+  <a href="webshop.php"><p>o u r &nbsp&nbsp&nbsp w e b s h o p &nbsp&nbsp&nbsp></p></a>
+</div>
+
+<section id="resellers" class="white">
+
+     <h2>Our<br/>Resellers:</h2>
+     <p>
+        Sunny uses resellers.<br/>
+        You can buy your socks from these sites:
+     </p>
+
+ 
+     <div id="germanResell">
+         <h3>in<br/>Germany:</h3>
+         <a href="https://www.amazon.de">amazon.de ></a>
+     </div>
+
+     <div id="dutchResell">
+        <h3>in the<br/>Netherlands:</h3>
+        <a href="https://www.hema.com">hema.com ></a>
+        <br/>
+        <a href="https://www.bol.com">bol.com ></a>
+     </div>
+
+</section>
+
+
+</body>
+</html>
