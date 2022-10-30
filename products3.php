dd0c243b717a810b3c72717fe1f10747d9bb09e5
beyza olmez <beyza.olmez@student.nhlstenden.com>
2022-10-30 18:15:29 +0100

    made the logo interactive

--- products3.php
+++ products3.php
@@ -12,7 +12,7 @@
         <div class="grid">
 
 
-            <img id="logo"src="img/sunny_logos_slogan_blue.png"  alt="logo">
+            <a href="index.html" id="logolink"> <img id="logo"src="img/sunny_logos_slogan_blue.png" alt="logo"> </a>
             <a href="index.html" class="toplink1">Home page</a>
             <a href="index.html#blueframe" class="toplink2">About Us</a>
             <a href="products1.php" class="toplink3">Products</a>
